17886a6e60676c7f30befe2709521bab73a7cb61
Yunus Emre Taşçı <yunusemretasci092@gmail.com>
2025-09-22 13:40:51 +0300

    feat: enhance todo functionality with checkbox and order todos by ID

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,18 @@ function App() {
   const [todos, setTodos] = useState([])
   const [text, setText] = useState("")
 
+  async function updateTodo(todo) {
+    await supabase.from('todos').update({done:!todo.done}).eq('id',todo.id)
+    await getTodos()
+  }
+  
   async function createTodo() {
     await supabase.from('todos').insert({title:text})
     await getTodos()
   }
 
   async function getTodos() {
-    const{data}=await supabase.from('todos').select()
+    const{data}=await supabase.from('todos').select().order('id')
     setTodos(data)
   }
   useEffect(() => {
@@ -32,9 +37,11 @@ function App() {
         placeholder="Insert the title"
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={()=>createTodo()}>Add</button>
+      <button onClick={createTodo}>Add</button>
       {todos.map((todo) => (
-        <p key={todo.id}>{todo.title}</p>
+        <p key={todo.id}>
+          <input type="checkbox" checked={todo.done} onChange={()=>updateTodo(todo)} />
+          {todo.title}</p>
       ))}
     </>
   );
