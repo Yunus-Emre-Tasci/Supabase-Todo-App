581250f3ec73ebbd8c6d51f3c900de882526a924
Yunus Emre Taşçı <yunusemretasci092@gmail.com>
2025-09-22 13:24:40 +0300

    feat: integrate Supabase for todo management and remove unused styles
    - Added @supabase/supabase-js dependency for backend integration.
    - Implemented functionality to create and fetch todos from Supabase.
    - Removed unused CSS files and styles to clean up the project.
    - Updated App component to include input for adding todos and display the list of todos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
 import './App.css'
+import { createClient } from '@supabase/supabase-js'
+import { useState } from 'react';
+
+const supabase = createClient(
+  "https://ebwmbdlhrlbbmdkhcomc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVid21iZGxocmxiYm1ka2hjb21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzOTAyOTMsImV4cCI6MjA3Mzk2NjI5M30.gM_PBbpGrjvZJr2g3W4VrmSQlAD-Bv1s-OQERiXD3Ys"
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState("")
+
+  async function createTodo() {
+    await supabase.from('todos').insert({title:text})
+    await getTodos()
+  }
+
+  async function getTodos() {
+    const{data}=await supabase.from('todos').select()
+    setTodos(data)
+  }
+  useEffect(() => {
+    getTodos()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        value={text}
+        placeholder="Insert the title"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={()=>createTodo()}>Add</button>
+      {todos.map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
     </>
-  )
-}
+  );
+} 
 
 export default App
